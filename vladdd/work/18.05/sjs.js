f31ec757837cc6338d1a8ae8867cbbea18ebd7ac
Vlad <wiking-007@yandex.ru>
2018-05-19 23:15:51 +0000

    work

---
+++ vladdd/work/18.05/sjs.js
@@ -0,0 +1,12 @@
+// JavaScript Filevar
+   
+   var a=0;
+   console.log(a);
+   var a=!a;
+   console.log(a);
+   var a=!"0";
+   console.log(a);
+   var a=!0;
+   console.log(a);
+   var a=!"";
+   console.log(a);
