bf87ab9394d9398182a45b1c2dbbbc2a008daf0a
Oksana Labeiko <oksi666@xaker.ru>
2018-05-21 16:55:40 +0000

    work 21.05

---
+++ oxana1510/work/21.05/script.js
@@ -0,0 +1,56 @@
+// JavaScript File
+var generator = [
+    ["песни", "гвозди", "трамваи"],
+    ["созвездия", "болезней", "бездарей"],
+    ["гончих псов", "бешенных белок", "летающих ложек"]
+];
+console.log(generator[0][0] + generator[1][0] + generator[2][0]);
+console.log(generator[0][0] + generator[1][0] + generator[2][1]);
+console.log(generator[0][0] + generator[1][0] + generator[2][2]);
+
+console.log(generator[0][0] + generator[1][1] + generator[2][0]);
+console.log(generator[0][0] + generator[1][1] + generator[2][1]);
+console.log(generator[0][0] + generator[1][1] + generator[2][2]);
+
+console.log(generator[0][0] + generator[1][2] + generator[2][0]);
+console.log(generator[0][0] + generator[1][2] + generator[2][1]);
+console.log(generator[0][0] + generator[1][2] + generator[2][2]);
+
+
+console.log(generator[0][1] + generator[1][0] + generator[2][0]);
+console.log(generator[0][1] + generator[1][0] + generator[2][1]);
+console.log(generator[0][1] + generator[1][0] + generator[2][2]);
+
+
+console.log(generator[0][1] + generator[1][1] + generator[2][0]);
+console.log(generator[0][1] + generator[1][1] + generator[2][1]);
+console.log(generator[0][1] + generator[1][1] + generator[2][2]);
+
+console.log(generator[0][1] + generator[1][2] + generator[2][0]);
+console.log(generator[0][1] + generator[1][2] + generator[2][1]);
+console.log(generator[0][1] + generator[1][2] + generator[2][2]);
+
+
+console.log(generator[0][2] + generator[1][0] + generator[2][0]);
+console.log(generator[0][2] + generator[1][0] + generator[2][1]);
+console.log(generator[0][2] + generator[1][0] + generator[2][2]);
+
+
+console.log(generator[0][2] + generator[1][1] + generator[2][0]);
+console.log(generator[0][2] + generator[1][1] + generator[2][1]);
+console.log(generator[0][2] + generator[1][1] + generator[2][2]);
+
+console.log(generator[0][2] + generator[1][2] + generator[2][0]);
+console.log(generator[0][2] + generator[1][2] + generator[2][1]);
+console.log(generator[0][2] + generator[1][2] + generator[2][2]);
+
+
+
+
+
+for (var i = 0; i <= 10; i++) {
+    console.log(i)
+}
+for (var i = 0; i <= 10; i) {
+    console.log(++i)
+}
