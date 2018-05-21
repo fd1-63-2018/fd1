173d183932494e9b7eb66b34ab4dfe4a85c7b268
Alex Roma <medikroma@gmail.com>
2018-05-21 14:52:05 +0000

    ++ili++

--- medrom4/work/21.05/script.js
+++ medrom4/work/21.05/script.js
@@ -3,33 +3,44 @@ var generator = [
     ["созвездие", "болезней", "бездарей"],
     ["гончих псов", "бешеных белок", "летающих ложек"]
 ];
-console.log(generator.length);
-alert(generator[0][0] +" "+ generator[1][0] +" "+ generator[2][0]);
-alert(generator[0][0] +" "+ generator[1][0] +" "+ generator[2][1]);
-alert(generator[0][0] +" "+ generator[1][0] +" "+ generator[2][2]);
-alert(generator[0][0] +" "+ generator[1][1] +" "+ generator[2][0]);
-alert(generator[0][0] +" "+ generator[1][1] +" "+ generator[2][2]);
-alert(generator[0][0] +" "+ generator[1][1] +" "+ generator[2][2]);
-alert(generator[0][0] +" "+ generator[1][2] +" "+ generator[2][0]);
-alert(generator[0][0] +" "+ generator[1][2] +" "+ generator[2][1]);
-alert(generator[0][0] +" "+ generator[1][2] +" "+ generator[2][2]);
 
-alert(generator[0][1] +" "+ generator[1][0] +" "+ generator[2][0]);
-alert(generator[0][1] +" "+ generator[1][0] +" "+ generator[2][1]);
-alert(generator[0][1] +" "+ generator[1][0] +" "+ generator[2][2]);
-alert(generator[0][1] +" "+ generator[1][1] +" "+ generator[2][0]);
-alert(generator[0][1] +" "+ generator[1][1] +" "+ generator[2][2]);
-alert(generator[0][1] +" "+ generator[1][1] +" "+ generator[2][2]);
-alert(generator[0][1] +" "+ generator[1][2] +" "+ generator[2][0]);
-alert(generator[0][1] +" "+ generator[1][2] +" "+ generator[2][1]);
-alert(generator[0][1] +" "+ generator[1][2] +" "+ generator[2][2]);
+alert(generator[0][0] + " " + generator[1][0] + " " + generator[2][0]);
+alert(generator[0][0] + " " + generator[1][0] + " " + generator[2][1]);
+alert(generator[0][0] + " " + generator[1][0] + " " + generator[2][2]);
+alert(generator[0][0] + " " + generator[1][1] + " " + generator[2][0]);
+alert(generator[0][0] + " " + generator[1][1] + " " + generator[2][2]);
+alert(generator[0][0] + " " + generator[1][1] + " " + generator[2][2]);
+alert(generator[0][0] + " " + generator[1][2] + " " + generator[2][0]);
+alert(generator[0][0] + " " + generator[1][2] + " " + generator[2][1]);
+alert(generator[0][0] + " " + generator[1][2] + " " + generator[2][2]);
 
-alert(generator[0][2] +" "+ generator[1][0] +" "+ generator[2][0]);
-alert(generator[0][2] +" "+ generator[1][0] +" "+ generator[2][1]);
-alert(generator[0][2] +" "+ generator[1][0] +" "+ generator[2][2]);
-alert(generator[0][2] +" "+ generator[1][1] +" "+ generator[2][0]);
-alert(generator[0][2] +" "+ generator[1][1] +" "+ generator[2][2]);
-alert(generator[0][2] +" "+ generator[1][1] +" "+ generator[2][2]);
-alert(generator[0][2] +" "+ generator[1][2] +" "+ generator[2][0]);
-alert(generator[0][2] +" "+ generator[1][2] +" "+ generator[2][1]);
-alert(generator[0][2] +" "+ generator[1][2] +" "+ generator[2][2]);
+alert(generator[0][1] + " " + generator[1][0] + " " + generator[2][0]);
+alert(generator[0][1] + " " + generator[1][0] + " " + generator[2][1]);
+alert(generator[0][1] + " " + generator[1][0] + " " + generator[2][2]);
+alert(generator[0][1] + " " + generator[1][1] + " " + generator[2][0]);
+alert(generator[0][1] + " " + generator[1][1] + " " + generator[2][2]);
+alert(generator[0][1] + " " + generator[1][1] + " " + generator[2][2]);
+alert(generator[0][1] + " " + generator[1][2] + " " + generator[2][0]);
+alert(generator[0][1] + " " + generator[1][2] + " " + generator[2][1]);
+alert(generator[0][1] + " " + generator[1][2] + " " + generator[2][2]);
+
+alert(generator[0][2] + " " + generator[1][0] + " " + generator[2][0]);
+alert(generator[0][2] + " " + generator[1][0] + " " + generator[2][1]);
+alert(generator[0][2] + " " + generator[1][0] + " " + generator[2][2]);
+alert(generator[0][2] + " " + generator[1][1] + " " + generator[2][0]);
+alert(generator[0][2] + " " + generator[1][1] + " " + generator[2][2]);
+alert(generator[0][2] + " " + generator[1][1] + " " + generator[2][2]);
+alert(generator[0][2] + " " + generator[1][2] + " " + generator[2][0]);
+alert(generator[0][2] + " " + generator[1][2] + " " + generator[2][1]);
+alert(generator[0][2] + " " + generator[1][2] + " " + generator[2][2]);
+
+
+var generator = [
+    ["песни", "гвозди", "трамвай"],
+    ["созвездие", "болезней", "бездарей"],
+    ["гончих псов", "бешеных белок", "летающих ложек"]
+];
+
+for (var generator = 0; generator <= 10;) {
+    console.log(++generator);
+}
