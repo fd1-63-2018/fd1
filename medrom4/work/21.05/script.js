bdda5d32b2b6a72c85ea122ac619dcd64be9fe50
Alex Roma <medikroma@gmail.com>
2018-05-21 14:27:32 +0000

    PesniSozvezdiiGonhixPsov

---
+++ medrom4/work/21.05/script.js
@@ -0,0 +1,35 @@
+var generator = [
+    ["песни", "гвозди", "трамвай"],
+    ["созвездие", "болезней", "бездарей"],
+    ["гончих псов", "бешеных белок", "летающих ложек"]
+];
+console.log(generator.length);
+alert(generator[0][0] +" "+ generator[1][0] +" "+ generator[2][0]);
+alert(generator[0][0] +" "+ generator[1][0] +" "+ generator[2][1]);
+alert(generator[0][0] +" "+ generator[1][0] +" "+ generator[2][2]);
+alert(generator[0][0] +" "+ generator[1][1] +" "+ generator[2][0]);
+alert(generator[0][0] +" "+ generator[1][1] +" "+ generator[2][2]);
+alert(generator[0][0] +" "+ generator[1][1] +" "+ generator[2][2]);
+alert(generator[0][0] +" "+ generator[1][2] +" "+ generator[2][0]);
+alert(generator[0][0] +" "+ generator[1][2] +" "+ generator[2][1]);
+alert(generator[0][0] +" "+ generator[1][2] +" "+ generator[2][2]);
+
+alert(generator[0][1] +" "+ generator[1][0] +" "+ generator[2][0]);
+alert(generator[0][1] +" "+ generator[1][0] +" "+ generator[2][1]);
+alert(generator[0][1] +" "+ generator[1][0] +" "+ generator[2][2]);
+alert(generator[0][1] +" "+ generator[1][1] +" "+ generator[2][0]);
+alert(generator[0][1] +" "+ generator[1][1] +" "+ generator[2][2]);
+alert(generator[0][1] +" "+ generator[1][1] +" "+ generator[2][2]);
+alert(generator[0][1] +" "+ generator[1][2] +" "+ generator[2][0]);
+alert(generator[0][1] +" "+ generator[1][2] +" "+ generator[2][1]);
+alert(generator[0][1] +" "+ generator[1][2] +" "+ generator[2][2]);
+
+alert(generator[0][2] +" "+ generator[1][0] +" "+ generator[2][0]);
+alert(generator[0][2] +" "+ generator[1][0] +" "+ generator[2][1]);
+alert(generator[0][2] +" "+ generator[1][0] +" "+ generator[2][2]);
+alert(generator[0][2] +" "+ generator[1][1] +" "+ generator[2][0]);
+alert(generator[0][2] +" "+ generator[1][1] +" "+ generator[2][2]);
+alert(generator[0][2] +" "+ generator[1][1] +" "+ generator[2][2]);
+alert(generator[0][2] +" "+ generator[1][2] +" "+ generator[2][0]);
+alert(generator[0][2] +" "+ generator[1][2] +" "+ generator[2][1]);
+alert(generator[0][2] +" "+ generator[1][2] +" "+ generator[2][2]);
