66b0315ac1b7797f8298b89d39e2eecf1632e910
Marat <santinel_1991@mail.ru>
2018-05-21 14:34:22 +0000

    21.05

--- Marat/work/21.05/script.js
+++ Marat/work/21.05/script.js
@@ -18,7 +18,7 @@ alert(generator[0][0] +" "+ generator[1][2] + generator[2][2]);
 
 alert(generator[0][1] +" "+ generator[1][0] + generator[2][0]);
 alert(generator[0][1] +" "+ generator[1][0] + generator[2][1]);
-alert(generator[0][1] +" "+ generator[1][0] + generator[2][2]);
+alert(generator[0][1] +" "+ generator[1][0] + generator[2][2]);  
 alert(generator[0][1] +" "+ generator[1][1] + generator[2][0]);
 alert(generator[0][1] +" "+ generator[1][1] + generator[2][2]);
 alert(generator[0][1] +" "+ generator[1][1] + generator[2][2]);
