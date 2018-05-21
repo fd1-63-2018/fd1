bb50adc7727cb18e6eb672e5900e01421eb2fd77
Tatiana <tanivetka@yandex.by>
2018-05-21 14:04:45 +0000

    fix old tasks

---
+++ Tanivetka/work/18.05/script.js
@@ -0,0 +1,12 @@
+
+        var a=0;
+        console.log(a);
+        a=!a;
+        console.log(a);
+        a=!'0';
+        console.log(a);
+        a=!0;
+        console.log(a);
+        a=!'';
+        console.log(a);
+        
